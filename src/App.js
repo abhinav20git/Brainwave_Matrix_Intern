3e922de28968c596278deab59d80189f392de650
abhinav20git <118235094+abhinav20git@users.noreply.github.com>
2024-09-15 22:51:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react"
 
 function App() {
+  const arr=["itrms1","itrms2","itrms3","itrms4"]
+  const arr1=[1,2,3,4,5,6,7,8,9,10]
+  const arr2=[2,3,4,5,20,10]
+  const [inputValue,setInputValue]=useState([])
+
+  function evenodd(num){
+    if(num%2===0){
+      return num;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{inputValue}</p> 
+      <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} />
+
+      {arr.map((item,i)=>{
+        
+         return( 
+          <ol>
+         <li >{i}{item}</li>
+         </ol>
+        )
+})}
+<li>{
+  arr1.filter((num)=>evenodd(num))
+
+}</li>
+
+<p>
+  {
+    arr2.reduce((acc,item)=>{
+      console.log(acc)
+      console.log(item)
+    return acc=acc+item;
+  })
+  }
+</p>
+
+
+
     </div>
   );
 }
